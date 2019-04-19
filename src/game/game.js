badcbc7bef52535454d43abfb529be77ae6b51d8
Tori Rojas <tori@tensoriot.com>
2019-04-19 11:45:42 -0700

    cleaning code, adding comments

--- src/game/game.js
+++ src/game/game.js
@@ -3,6 +3,8 @@ import './game.css';
 import { Button, Card } from '@material-ui/core';
 
 class Game extends Component {
+
+    // basic constructor to set state of letter pool and possible words
     constructor(props) {
         super(props)
         this.state = {
@@ -14,57 +16,43 @@ class Game extends Component {
             isLoading: true
         }
     }
+    
+    // when the component mounts, a function is triggered to select a word
+    componentDidMount = () => {
+        this.selectWord()
+    }
 
+    // selects a random word from the array of possible words
     selectWord = () => {
-        const possibleNums = this.state.possibleWords.length
+        const possibleNums = this.state.possibleWords.length        
+        // triggers a random number to be generated from 0 - # of possible words
         let randomNum = Math.floor(Math.random() * possibleNums)
+        // sets the chosen word to equal the word indexed in the array of possible words
         this.setState({
-            chosenWord: this.state.possibleWords[randomNum],
+            chosenWord: this.state.possibleWords[randomNum].toUpperCase().split(''),
+        // triggers function that converts the alphabet string into an array
         }, () => this.splitRemainingLetters())
     }
 
+    // splits the remainingLetters string into an array
+    // 'cause ain't nobody got time for that
     splitRemainingLetters = () => {
         let remainingLetters = this.state.remainingLetters;
         remainingLetters = remainingLetters.split('');
         this.setState({ 
             remainingLetters: remainingLetters, 
             isLoading: false 
-        }, this.updateWordView());
+        // triggers function that replaces letters with dashes
+        }, this.createAllDashWord());
     }
 
-    componentDidMount = () => {
-        this.selectWord()
-    }
-
-    chooseLetter = (event) => {
-        const value = event.currentTarget.value;
-        let remainingLetters = this.state.remainingLetters;
-        let remainingChances = this.state.remainingChances;
-        let incorrectLetters = this.state.incorrectLetters;
-        let letterIndex = remainingLetters.indexOf(value);
-        const letterIncluded = this.state.chosenWord.toUpperCase().includes(value)
-        console.log('letterIncluded', letterIncluded)
-        remainingLetters.splice(letterIndex, 1);
-        if (letterIncluded === false) {
-            incorrectLetters.push(value);            
-        }
-        remainingChances = remainingChances - 1;
-        if (remainingChances === 0) {
-            alert("You Lost!")
-            window.location.reload();
-        }
-        this.setState({
-            isLoading: true,
-            remainingLetters: remainingLetters,
-            incorrectLetters: incorrectLetters,
-            remainingChances: remainingChances,
-            chosenLetter: value
-        }, () => this.updateWordView())
-    }
-
-    createAllDashWord = (word) => {
+    // replaces the chosen word's letters with dashes 
+    createAllDashWord = () => {
+        const word = this.state.chosenWord;
         let wordView = []
-        word.map((letter) => {
+        // for each item that exists in the chosenWord array, 
+        //      a '-' is pushed into the wordView array
+        word.map(() => {
             wordView.push('-')
             return wordView
         })
@@ -73,28 +61,71 @@ class Game extends Component {
         })
     }
 
-    replaceDashWithLetter = (word, chosenLetter) => {
-        let chosenWordView = this.state.chosenWordView
-        const letterIndex = word.indexOf(chosenLetter)
-        chosenWordView.splice(letterIndex, 1, chosenLetter);
+    chooseLetter = (event) => {
+        const value = event.currentTarget.value;
         this.setState({
-            chosenWordView: chosenWordView,
-            isLoading: false 
-        })
+            isLoading: true,
+            remainingChances: this.updateRemainingChances(),
+            chosenLetter: value
+        }, () => this.updateWordView())
+    }
+
+    updateRemainingChances = () => {
+        let remainingChances = this.state.remainingChances;
+        remainingChances = remainingChances - 1;
+        if (remainingChances === -1) {
+            this.restartGame('You Lost!')
+        }
+        return remainingChances;
     }
 
     updateWordView = () => {
-        console.log(this.state)
         let chosenLetter = this.state.chosenLetter;
-        let word = this.state.chosenWord.toUpperCase();
-        word = word.split('');
-        if (!this.state.chosenLetter) {
-            this.createAllDashWord(word)
-        } else if (word.includes(chosenLetter)) {
+        let word = this.state.chosenWord;
+        if (word.includes(chosenLetter)) {
             this.replaceDashWithLetter(word, chosenLetter)
         } else {
-            this.setState({ isLoading: false })
+            this.updateIncorrectLetters(chosenLetter)
         }
+    }
+
+    replaceDashWithLetter = (word, chosenLetter) => {
+        let chosenWordView = this.state.chosenWordView;
+        const letterIndex = word.indexOf(chosenLetter);
+        chosenWordView.splice(letterIndex, 1, chosenLetter);
+        this.setState({
+            chosenWordView: chosenWordView
+        }, () => {
+            if (!chosenWordView.includes('-')) {
+                this.restartGame('Congratulations! You Won! The word is ' + this.state.chosenWord.join(''))
+            } else {
+                this.updateRemainingLetters()
+            }
+        })
+    }
+
+    updateIncorrectLetters = (chosenLetter) => {
+        let incorrectLetters = this.state.incorrectLetters;
+        incorrectLetters.push(chosenLetter);         
+        this.setState({
+            incorrectLetters: incorrectLetters
+        }, () => this.updateRemainingLetters())
+    }
+    
+    updateRemainingLetters = () => {
+        let remainingLetters = this.state.remainingLetters;
+        let chosenLetter = this.state.chosenLetter;
+        let letterIndex = remainingLetters.indexOf(chosenLetter);
+        remainingLetters.splice(letterIndex, 1);
+        this.setState({
+            remainingLetters: remainingLetters,
+            isLoading: false
+        })
+    }
+
+    restartGame = (message) => {
+        alert(message)
+        window.location.reload();
     }
 
     render() {
